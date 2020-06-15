51b09a8444294703c155311f8ea07b763ebd3ac7
Sam Mburu <sammxuser@gmail.com>
2020-06-15 21:56:48 +0000

    Initial Commit

---
+++ webapp/controller/login.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sampleProject.zSampleProject.controller.login", {
+		onInit: function () {
+
+		},
+		handleogin: function(){
+			
+		}
+	});
+});
