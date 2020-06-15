d7d4acebcd119a446d571ef050f401a846340cff
Sam Mburu <sammxuser@gmail.com>
2020-06-15 21:58:37 +0000

    initial commit

--- webapp/controller/login.controller.js
+++ webapp/controller/login.controller.js
@@ -1,14 +1,27 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("sampleProject.zSampleProject.controller.login", {
 		onInit: function () {
 
 		},
-		handleogin: function(){
-			
+		handleLogin: function () {
+			var username = this.byId("inpUsername");
+			var password = this.byId("inpPassword");
+
+			if (username.getValue() === "" || password.getValue() === "") {
+				username.setValueState("Error");
+				password.setValueState("Error");
+
+				MessageBox.show(" Fields marked with a * are mandatory ", {
+					title: " Warming Message ",
+					icon: MessageBox.Icon.ERROR
+				});
+
+			}
 		}
 	});
 });
